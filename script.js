7460f0c5d5a2abaffe31d945c4e7b4a20cdfc5a4
aryy8 <aryanfeb17@gmail.com>
2024-11-16 01:46:11 +0530

    .

--- script.js
+++ script.js
@@ -1,8 +1,2 @@
-// script.js
 
-document.addEventListener("DOMContentLoaded", function() {
-    const projectsSection = document.getElementById('projects');
-    const message = document.createElement('p');
-    message.textContent = "Check out my latest projects below!";
-    projectsSection.prepend(message);
-});
+
